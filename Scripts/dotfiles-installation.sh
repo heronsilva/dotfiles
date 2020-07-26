08b2ab459bd886b5229f8930381c6ee973c53e4f
Heron Silva <heron.code@outlook.com>
2020-07-26 15:59:07 -0300

    add dotfiles installation script
    forked from this attlasian tutorial:
      https://www.atlassian.com/git/tutorials/dotfiles

---
+++ Scripts/dotfiles-installation.sh
@@ -0,0 +1,21 @@
+git clone --bare git@bitbucket.org:durdn/cfg.git $HOME/.cfg
+
+function config {
+  /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+mkdir -p .dotfiles-backup
+
+config checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out .";
+else
+  echo "Backing up pre-existing dot files.";
+
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+
+config checkout
+config config status.showUntrackedFiles no
+
