56378288f4f7e6cc0fe1b73e750ad8c5fb72aa5d
Heron Silva <heron.code@outlook.com>
2020-07-26 22:35:47 -0300

    refactor installation script

--- Scripts/dotfiles-installation.sh
+++ Scripts/dotfiles-installation.sh
@@ -1,19 +1,35 @@
-git clone --bare https://github.com/heronsilva/dotfiles.git $HOME/.dotfiles
+#!/bin/bash
 
-function config {
-  /usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME $@
-}
+REPO_URL=https://github.com/heronsilva/dotfiles.git
+DEST_PATH=$HOME/.dotfiles
+BACKUP_PATH=$HOME/.dotfiles-backup
 
-mkdir -p .dotfiles-backup
+rm -rf ~/.dotfiles
+rm -rf ~/.dotfiles-backup
+
+git clone --bare $REPO_URL $DEST_PATH 
+
+alias config=/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME
 
 config checkout
 
 if [ $? = 0 ]; then
-  echo "Checked out .";
+  echo "Checked out .dotfiles";
 else
-  echo "Backing up pre-existing dot files.";
+  echo "Backing up pre-existing dot files";
+  mkdir -p BACKUP_PATH 
 
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+  files=`config checkout 2>&1 | egrep "\s+\." | awk {'print $1'}`
+
+  for file in $files
+  do
+    directory=`dirname $BACKUP_PATH/$file`
+    echo 'Creating directory' $directory
+    mkdir --parents $directory
+
+    echo 'Moving file ' $file
+    mv $file $BACKUP_PATH
+  done
 fi;
 
 config checkout
