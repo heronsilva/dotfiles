56d9d0046356cfe7e7b9707d6ca2ac145c23f051
Heron Silva <9641348+heronsilva@users.noreply.github.com>
2024-11-05 00:10:21 -0300

    add dependency installation script for linux and osx

--- bootstrap/dependencies.sh
+++ bootstrap/dependencies.sh
@@ -7,6 +7,88 @@ function confirm() {
 	return 0
 }
 
+# install dependencies
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	echo "Installing linux dependencies"
+
+	DISTRO=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
+	if [ $DISTRO == "arch" ] || [ $DISTRO == "manjaro" ]; then
+		sudo pacman -Syu --noconfirm
+		sudo pacman -S --noconfirm --needed \
+			alacritty \
+			base-devel \
+			bat \
+			curl \
+			fzf \
+			gcc \
+			git \
+			htop \
+			jq \
+			neovim \
+			tmux \
+			vim \
+			wget \
+			zsh
+	elif [ $DISTRO == "ubuntu" ] || [ $DISTRO == "debian" ]; then
+		sudo apt update
+		sudo apt install -y \
+			asdf \
+			bat \
+			docker \
+			fzf \
+			gawk \
+			gh \
+			git \
+			jq \
+			neovim \
+			tmux \
+			trash-cli
+		sudo apt install -y \
+			alacritty \
+			fig \
+			fonts-iosevka \
+			fonts-jetbrains-mono \
+			raycast \
+			warp
+	else
+		echo "Unknown distro"
+	fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "Installing OSX dependencies"
+
+	# homebrew
+	if [ ! -f /opt/homebrew/bin/brew ] && confirm "homebrew"; then
+		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	fi
+
+	brew update
+	brew install \
+		asdf \
+		bat \
+		docker \
+		docker-completion \
+		fzf \
+		gawk \
+		gh \
+		git \
+		jq \
+		neovim \
+		tmux \
+		trash-cli
+	brew install --cask \
+		alacritty \
+		fig \
+		font-iosevka \
+		font-iosevka-nerd-font \
+		font-jetbrains-mono \
+		font-jetbrains-mono-nerd-font \
+		raycast \
+		warp
+else
+	echo "unknown os"
+fi
+
+# install user dependencies
 if confirm "oh-my-zsh"; then
 	curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh
 	printf "Done installing oh-my-zsh \n"
