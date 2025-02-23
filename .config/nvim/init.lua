2a28e06af40ed0bdfad37e43025eb3c66f05fc48
Heron Silva <heron.code@outlook.com>
2025-02-23 19:22:08 -0300

    nvim: update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,8 @@
--- https://github.com/garcia5/dotfiles/blob/master/files/nvim/init.lua
-
-require("config.options")
-
 if vim.loader then
-  vim.loader.enable() -- cache lua modules (https://github.com/neovim/neovim/pull/22668)
+    vim.loader.enable() -- cache lua modules (https://github.com/neovim/neovim/pull/22668)
 end
 
+require("config.options")
 require("config.lazy")
+require("config.autocmd")
 require("config.keymaps")
-
-vim.cmd("colorscheme gruvbox")
