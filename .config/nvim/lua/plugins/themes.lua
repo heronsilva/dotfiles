4f3fdf7ee25605f9cda4dcb10567c71adfb83434
Heron Silva <heron.code@outlook.com>
2025-02-23 19:13:04 -0300

    nvim: add some colorschemes

---
+++ .config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,12 @@
+vim.keymap.set("n", "<leader>tt", persistent_colorscheme)
+
+return {
+    { "ellisonleao/gruvbox.nvim", name = "gruvbox" },
+    { "catppuccin/nvim", name = "catppuccin" },
+    { "shaunsingh/nord.nvim", name = "nord" },
+    { "rose-pine/neovim", name = "rose-pine" },
+    { "folke/tokyonight.nvim", name = "tokyonight" },
+    { "rebelot/kanagawa.nvim", name = "kanagawa" },
+    { "Mofiqul/dracula.nvim", name = "dracula" },
+    { "maxmx03/solarized.nvim", name = "solarized" },
+}
