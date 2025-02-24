c94f80f33cf470f5ef7eb25705d140458b121b5d
Heron Silva <heron.code@outlook.com>
2025-02-23 22:25:57 -0300

    nvim: enhance treesitter config

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,63 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.install").prefer_git = true
-    local treesitter = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "diff",
+                "dockerfile",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "luadoc",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "sql",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+            auto_install = true,
+            sync_install = false,
+            highlight = {
+                enable = true,
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024 -- 100 KB
+                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                    if ok and stats and stats.size > max_filesize then
+                        return true
+                    end
+                end,
+                additional_vim_regex_highlighting = true,
+            },
+            indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            autotag = { enable = true },
+            ignore_install = {},
+            modules = {},
+        })
+    end,
 
-    treesitter.setup({
-      highlight = { enable = true },
-      indent = { enable = true },
-      autotag = { enable = true },
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "yaml",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        }
-      }
-    })
-  end
+    -- There are additional nvim-treesitter modules that you can use to interact
+    -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --
+    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 }
