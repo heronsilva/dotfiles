d5bfc3573bc400f207bf9508593f66052883a25e
Heron S. Silva <heron.code@outlook.com>
2025-10-18 15:08:01 -0300

    zsh: add custom implementation of globalias plugin

--- home/.zsh/plugins.zsh
+++ home/.zsh/plugins.zsh
@@ -29,9 +29,10 @@ is_installed podman && zinit snippet OMZ::plugins/podman/podman.plugin.zsh
 zinit snippet OMZ::plugins/extract/extract.plugin.zsh
 zinit snippet OMZ::plugins/command-not-found/command-not-found.plugin.zsh
 zinit snippet OMZ::plugins/colored-man-pages/colored-man-pages.plugin.zsh
-zinit snippet OMZ::plugins/globalias/globalias.plugin.zsh # expands glob expressions, subcommands and aliases
-
 zinit snippet OMZ::plugins/mise/mise.plugin.zsh
+# zinit snippet OMZ::plugins/globalias/globalias.plugin.zsh # expands glob expressions, subcommands and aliases
+# my patched versions of globalias, to prevent expanding ~, l, ll, etc
+zinit light-mode for ~/.zsh/plugins/globalias
 
 # Performance-related
 zinit ice wait lucid atinit"zpcompinit; zpcdreplay"
