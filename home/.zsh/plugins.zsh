85760d5aa86510511cf9e74f5c0ebbc4329fa101
Heron S. Silva <heron.code@outlook.com>
2025-10-18 15:11:35 -0300

    zsh: reorganize plugins

--- home/.zsh/plugins.zsh
+++ home/.zsh/plugins.zsh
@@ -5,10 +5,6 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 source "${ZINIT_HOME}/zinit.zsh"
 
 # Load zinit annexes
-zinit snippet OMZ::lib/compfix.zsh
-zinit snippet OMZ::lib/clipboard.zsh
-zinit snippet OMZ::lib/git.zsh
-
 zinit light zdharma-continuum/zinit-annex-as-monitor
 zinit light zdharma-continuum/zinit-annex-patch-dl
 zinit light zdharma-continuum/zinit-annex-rust
@@ -22,6 +18,9 @@ zinit light zsh-users/zsh-completions
 # zinit light MichaelAquilina/zsh-you-should-use
 
 # OMZ plugins and utilities
+zinit snippet OMZ::lib/compfix.zsh
+zinit snippet OMZ::lib/clipboard.zsh
+zinit snippet OMZ::lib/git.zsh
 is_installed git && zinit snippet OMZ::plugins/git/git.plugin.zsh
 is_installed docker && is_not_alias docker && zinit snippet OMZ::plugins/docker/docker.plugin.zsh
 is_installed podman && zinit snippet OMZ::plugins/podman/podman.plugin.zsh
