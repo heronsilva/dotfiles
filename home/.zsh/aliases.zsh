ea90d1aa74df254b9599db915255f1c16018d974
Heron S. Silva <heron@airtm.io>
2026-02-16 02:58:10 -0300

    refactor: update paths, remove outdate script

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -67,6 +67,12 @@ alias //="brew update && brew upgrade && brew cleanup"
 alias vimrc="vim $HOME/.vimrc"
 alias wezrc="nvim $HOME/.config/wezterm/wezterm.lua"
 
+# clear scrollback. see: https://apple.stackexchange.com/a/113168
+alias c="clear && printf '\e[3J'"
+
+# deduplicate history
+alias histclean='nl ~/.bash_history | sort -k 2  -k 1,1nr| uniq -f 1 | sort -n | cut -f 2 > unduped_history && cp unduped_history ~/.bash_history'
+
 #
 # Airtm
 alias le="local-env-palkia"
